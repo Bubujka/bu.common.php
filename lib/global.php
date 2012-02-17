47badab13a915cdfa2ca692b5e4957dd11e9a7a2
Aleksei Kaminin <zendzirou@gmail.com>
2012-02-17 23:27:15 +0400

    Added ob function

--- lib/global.php
+++ lib/global.php
@@ -5,3 +5,13 @@ def('is_closure', function($arg){
         return (is_object($arg) and (get_class($arg) == 'Closure')) ? true : false;
 });
 
+
+
+doc('выполняет функцию и возвращает всё что она напечатала в stdout в виде строки');
+def('ob', function($fn){
+        ob_start();
+        $fn();
+        $t = ob_get_contents();
+        ob_end_clean();
+        return $t;
+});
