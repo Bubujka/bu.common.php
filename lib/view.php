9d5d968abe824ee2d98c266dc2e9097abba087c7
Aleksei Kaminin <zendzirou@gmail.com>
2012-02-17 23:43:09 +0400

    Added function for handle tpls

---
+++ lib/view.php
@@ -0,0 +1,35 @@
+<?php
+
+doc('путь до папки с шаблонами.');
+def_return('view_pth', 'view');
+
+doc('загружает php файл из папки view_pth() и возвращает результат работы в виде строки.');
+def('view', function($pth, $args = array()){
+        if(func_num_args() == 2)
+                extract(func_get_arg(1));
+        require view_pth().'/'.$pth.'.php';
+});
+def_wrapper('view', 'ob'); 
+
+doc('загружает шаблон, передавая в него одну переменную с именем $data.');
+def('dview', function($pth, $data){
+        return view($pth, array('data'=>$data));
+});
+
+doc('обойти массив, для каждого его элемента загрузить шаблон и объединить 
+всё в одну строку');
+def('map_view', function($pth, $array, $imploder = ''){
+        $fn = function($v) use($pth){ 
+                return view($pth, $v);
+        };
+        return implode($imploder, array_map($fn, $array));
+});
+
+doc('обойти массив, для каждого его элемента загрузить шаблон и объединить 
+всё в одну строку, но при этом каждый элемент передавать в переменной $data');
+def('map_dview', function($pth, $array, $imploder = ''){
+        $fn = function($v) use($pth){ 
+                return dview($pth, $v);
+        };
+        return implode($imploder, array_map($fn, $array));
+});
