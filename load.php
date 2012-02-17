0d79aaaed4299f4323e667a3bc85465aa3623ccb
Aleksei Kaminin <zendzirou@gmail.com>
2012-02-17 22:14:19 +0400

    Added bu.defun as submodule

--- load.php
+++ load.php
@@ -1,11 +1,4 @@
 <?php
-if(!function_exists('bu\def\def'))
-        if(file_exists($it = '/home/lib/bu.defun/load.php'))
-                require_once $it;
-        else
-                exit("bu.defun not loaded!");
-        
-
-
+require_once 'bu.defun/load.php';
 foreach(glob(__DIR__.'/lib/*.php') as $v)
         require_once $v;
